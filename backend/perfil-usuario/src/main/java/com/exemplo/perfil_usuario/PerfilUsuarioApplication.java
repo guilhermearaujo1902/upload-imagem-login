4793144181eab9f8240a9429f53ff58ba0612ad3
Guilherme Araujo <gui.araujo1902@gmail.com>
2024-08-24 13:13:34 -0300

    feat(main): Criando projeto backend

---
+++ backend/perfil-usuario/src/main/java/com/exemplo/perfil_usuario/PerfilUsuarioApplication.java
@@ -0,0 +1,13 @@
+package com.exemplo.perfil_usuario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerfilUsuarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerfilUsuarioApplication.class, args);
+	}
+
+}
